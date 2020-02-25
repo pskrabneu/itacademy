283fc749eae693ca0daecef34d3499aec8bd2428
Pavel Skrabneu <skrabneu@gmail.com>
2020-02-25 18:43:59 +0300

    Tasks from 11 to 20. Doing some monkey job

--- src/main/java/by/htp/itacademy/linearprogramming/LinearProgrammingP2.java
+++ src/main/java/by/htp/itacademy/linearprogramming/LinearProgrammingP2.java
@@ -2,7 +2,16 @@ package by.htp.itacademy.linearprogramming;
 
 public class LinearProgrammingP2 {
     public static void main(String[] args) {
-
+//        task11();
+//        task12();
+//        task13();
+//        task14();
+//        task15();
+//        task16();
+//        task17();
+//        task18();
+//        task19();
+//        task20();
     }
 
     // Task #11
@@ -48,30 +57,58 @@ public class LinearProgrammingP2 {
 
     // Task #15
     // Написать программу, которая выводить на экран первые 4 степени числа "Пи"
+    private static void task15() {
+
+        System.out.println("\n>> Task #15");
+
+    }
 
 
 
     // Task #16
     // НАйти произведение цифр 4-значного числа
+    private static void task16(int x) {
+
+        System.out.println("\n>> Task #16");
+
+    }
+
 
     // Task #17
     // Даны 2 числа. Найти среднее арифметическое кубов этих чисел и среднее
     // геометрическое модулей этих чисел
+    private static void task17(int x, int y) {
+
+        System.out.println("\n>> Task #17");
+
+    }
 
 
     // Task #18
     // Дана длина ребра куба. Найти площадь грани,
     // площадь полной поверхности, и объем этого куба
-    //
+    private static void task18(double l) {
 
+        System.out.println("\n>> Task #18");
+
+    }
 
     // Task #19
     // Дана сторона равностороннего треугольника. Найти площадь этого
     // треугольника, его высоту, радиусы вписанной и описанной окружности
+    private static void task19(double a) {
+
+        System.out.println("\n>> Task #19");
+
+    }
 
 
     // Task #20
     // Дана длина окружности. Найти площадь круга, ограниченного
     // этой окружностью
+    private static void task20(double l) {
 
+        System.out.println("\n>> Task #20");
+
+    }
 }
