b3f4b8f9104a37af8173308fb885ce785cd8b989
Pavel Skrabneu <skrabneu@gmail.com>
2020-02-25 22:01:07 +0300

    Task 13 in draft

--- src/main/java/by/htp/itacademy/linearprogramming/LinearProgrammingP2.java
+++ src/main/java/by/htp/itacademy/linearprogramming/LinearProgrammingP2.java
@@ -1,10 +1,14 @@
 package by.htp.itacademy.linearprogramming;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.abs;
+
 public class LinearProgrammingP2 {
     public static void main(String[] args) {
-//        task11();
-//        task12();
-//        task13();
+//        task11(3,4);
+//        task12(3, 0, 0, 4);
+        task13(0, 0, 0, 6, 10, 0); //???
 //        task14();
 //        task15();
 //        task16();
@@ -19,8 +23,16 @@ public class LinearProgrammingP2 {
     // a и b двух катетов
     private static void task11(double a, double b) {
 
+        double c = sqrt(pow(a, 2) + pow(b, 2));
+
         System.out.println("\n>> Task #11");
 
+        System.out.println("The perimeter of triangle with\n" +
+                "a = " + a +
+                "\nb = " + b +
+                "\nc = " + c +
+                "\nis " + (a + b + c));
+        System.out.println("The square of triangle is " + ((a * b) / 2));
     }
 
 
@@ -30,7 +42,8 @@ public class LinearProgrammingP2 {
     private static void task12(double x1, double y1, double x2, double y2) {
 
         System.out.println("\n>> Task #12");
-
+        System.out.println("The distance between 2 points is " +
+                distance(x1, y1, x2, y2));
     }
 
 
@@ -41,8 +54,28 @@ public class LinearProgrammingP2 {
                               double x2, double y2,
                               double x3, double y3) {
 
-        System.out.println("\n>> Task #13");
+        double a;
+        double b;
+        double c;
+        double halfP;
 
+        a = distance(x1, y1, x2, y2);
+        b = distance(x2, y2, x3, y3);
+        c = distance(x3, y3, x1, y1);
+
+        System.out.println("\n>> Task #13");
+        System.out.println("a = " + a +
+                "b = " + b +
+                "c = " + c);
+
+        if (a == 0 || b == 0 || c == 0) {
+            halfP = (a + b + c) / 2;
+
+            System.out.println("The perimeter of the triangle is " + halfP * 2);
+            System.out.println("The area of the triangle is " + sqrt(halfP * (halfP - a) * (halfP - b) * (halfP - c)));
+        } else {
+            System.out.println("The figure with given coordinates is NOT triangle");
+        }
     }
 
 
@@ -110,5 +143,10 @@ public class LinearProgrammingP2 {
 
         System.out.println("\n>> Task #20");
 
+    }
+
+    // distance between 2 points with given coordinates
+    private static double distance(double x1, double y1, double x2, double y2) {
+        return sqrt(pow(abs(x1 - x2), 2) + pow(abs(y1 - y2), 2));
     }
 }
