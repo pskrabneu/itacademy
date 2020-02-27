00635a388797b9f8eb76303c03325350986dd2af
Pavel Skrabneu <skrabneu@gmail.com>
2020-02-27 12:57:08 +0300

    Task 21 started

--- src/main/java/by/htp/itacademy/linearprogramming/LinearProgrammingP3.java
+++ src/main/java/by/htp/itacademy/linearprogramming/LinearProgrammingP3.java
@@ -3,58 +3,119 @@ package by.htp.itacademy.linearprogramming;
 public class LinearProgrammingP3 {
     public static void main(String[] args) {
 
+        task21(123.456);
+//        task22();
+//        task23();
+//        task24();
+//        task25();
+//        task26();
+//        task27();
+//        task28();
+//        task29();
+//        task30();
+
     }
 
     // Task #21
     // Дано действительное (double) число R вида nnn.ddd (три цифровых
     // разряда в целой и дробной части). Поменять местами дробную
     // и целую части числа и вывести получнное значение.
+    private static void task21(double r) {
+
+        System.out.println("\n>> Task #21");
+
+    }
+
 
     // Task #22
     // Дано натуральное число "Т", которое представляет длительность
     // прошедшего времени в секундах. Вывести данное значение
     // длительности в часах, минутах и секундах в следующей форме:
     // HHч MMмин SSс
+    private static void task22(int t) {
+
+        System.out.println("\n>> Task #22");
+
+    }
 
 
     // Task #23
     // Найти площадь кольца, внутренний радиус которого равен
     // "r", а внешний -- "R" (R > r)
+    private static void task23(double rSmall, double rBig) {
 
+        System.out.println("\n>> Task #23");
+
+    }
 
 
     // Task #24
     // Найти площадь равнобедренной трапеции с основаниями "a" и "b"
     // и углом "alpha" при большем основании "a"
+    private static void task24(double a, double b, double alpha) {
 
+        System.out.println("\n>> Task #24");
 
+    }
 
 
     // Task #25
     // Вычислить корни квадратного уравнения a * x^2 + b * x + c = 0
     // с заданными коэффициентами a, b, c (предполагается, что a != 0 и
     // дискриминант уравнения неотрицателен)
+    private static void task25(int a, int b, int c) {
+
+        System.out.println("\n>> Task #25");
+
+    }
+
 
     // Task #26
     // НАйти площадь треугольника, две стороны которого равны "a"и "b",
     // а угол между сторонами "y"
+    private static void task26(double a, double b, double alpha) {
+
+        System.out.println("\n>> Task #26");
+
+    }
+
 
     // Task #27
     // Дано значение "a". Не используя никаких функций и никаких операций,
     // кроме умножения, получить значение a^3 за три операции и a^10 за четыре
     // операции
+    private static void task27(int a) {
+
+        System.out.println("\n>> Task #27");
+
+    }
+
 
     // Task #28
     // Составить программу перевода радианной меры угла в градусы, минуты, секунды
+    private static void task28(double alpha) {
 
+        System.out.println("\n>> Task #28");
+
+    }
 
 
     // Task #29
     // Найти (в радианах и градусах) все углы треугольника со сторонами
     // a, b, c
+    private static void task29(double a, double b, double c) {
+
+        System.out.println("\n>> Task #29");
+
+    }
 
 
     // Task #30
     // Три сопротивления "R1", "R2" и "R3" соединены параллельно
     // Найдите сопротивление соединения
+    private static void task30(double r1, double r2, double r3) {
+
+        System.out.println("\n>> Task #30");
+
+    }
 }
