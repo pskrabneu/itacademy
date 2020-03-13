7a610ae0165751b8f07ac637960dab09f3d3b76e
Pavel Skrabneu <skrabneu@gmail.com>
2020-03-13 20:29:49 +0300

    Decomposition Tasks06 done

--- src/main/java/by/htp/itacademy/decomposition/DecompositionTask06.java
+++ src/main/java/by/htp/itacademy/decomposition/DecompositionTask06.java
@@ -1,5 +1,7 @@
 package by.htp.itacademy.decomposition;
 
+import static by.htp.itacademy.util.Util.calculateAreaOfTriangle;
+
 
 // Task #06
 // 06. Вычислить площадь правильного шестиугольника со стороной а,
@@ -10,5 +12,11 @@ public class DecompositionTask06 {
         System.out.println("\n>> Task #06");
         // TODO
 
+        double a;
+
+        a = 6.25;
+
+        System.out.println("The area of hexagon with side " + a);
+        System.out.println("is " + (calculateAreaOfTriangle(a, a, a) * 6));
     }
 }
