6a5c51c1156369b20c7c6652462e233dd31209cf
Pavel Skrabneu <skrabneu@gmail.com>
2020-03-28 22:25:18 +0300

    Simple Classes and Objects -- Task01 completed

--- src/main/java/by/htp/itacademy/classesobjects/task01/Test1.java
+++ src/main/java/by/htp/itacademy/classesobjects/task01/Test1.java
@@ -6,4 +6,39 @@ package by.htp.itacademy.classesobjects.task01;
 //        из этих двух переменных.
 public class Test1 {
 
+    private int variable01;
+
+    private int variable02;
+
+    public int getVariable01() {
+        return variable01;
+    }
+
+    public void setVariable01(int variable01) {
+        this.variable01 = variable01;
+    }
+
+    public int getVariable02() {
+        return variable02;
+    }
+
+    public void setVariable02(int variable02) {
+        this.variable02 = variable02;
+    }
+
+    public void printVariables() {
+
+        System.out.println("Variable01 = " + this.variable01);
+        System.out.println("Variable02 = " + this.variable02);
+    }
+
+    public int getMaximum() {
+
+        return Math.max(this.variable01, this.variable02);
+    }
+
+    public int getSum() {
+
+        return this.variable01 + this.variable02;
+    }
 }
