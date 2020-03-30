b83e9c4466709deca977ff5bcdd0e6e13cb2517e
Pavel Skrabneu <skrabneu@gmail.com>
2020-03-30 19:18:54 +0300

    Simple Classes and Objects: Task03 -- in draft

--- src/main/java/by/htp/itacademy/classesobjects/task03/Student.java
+++ src/main/java/by/htp/itacademy/classesobjects/task03/Student.java
@@ -5,5 +5,48 @@ package by.htp.itacademy.classesobjects.task03;
 //номеров групп студентов, имеющих оценки, равные только 9 или 10.
 public class Student {
 
+    private String lastName;
+    private String initials;
 
+    private String groupNumber;
+
+    private int[] score = new int[5];
+
+    public Student(String lastName, String initials) {
+
+        this.lastName = lastName;
+        this.initials = initials;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getInitials() {
+        return initials.charAt(0) + ". " + initials.charAt(1) + ".";
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
+    public String getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(String groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+
+    public int[] getScore() {
+        return score;
+    }
+
+    public void setScore(int[] score) {
+        this.score = score;
+    }
 }
